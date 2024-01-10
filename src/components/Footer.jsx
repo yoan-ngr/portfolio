f2f263a870b641d2ad72723de79c1cef3b3fb8d6
yoan-ngr <yoan.nougueruiz@gmail.com>
2024-01-10 13:00:47 +0100

    Dates automatique

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,13 @@ import { Typography } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 
 function Footer () {
+
+    let date = new Date();
+
     return (
         <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t bg-blue-gray-50 dark:bg-zinc-800 px-6 dark:border-zinc-800 border-blue-gray-50 py-6 text-center md:justify-between">
             <Typography color="blue-gray" className="font-normal dark:text-blue-gray-100">
-                &copy; 2023 Yoan Nougué-Ruiz.
+                &copy; {date.getFullYear()} Yoan Nougué-Ruiz.
             </Typography>
             <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
                 <li>
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -5,6 +5,10 @@ import React from '../assets/technos/react.png';
 
 function About () {
 
+    let date = new Date();
+    let birth = new Date("2001-08-10");
+    let age = Math.abs(new Date(birth-date).getUTCFullYear() - 1970);
+
     return <div className={"mt-12"}>
         <div>
             <Typography variant="h1" className="text-center dark:text-zinc-50">À propos</Typography>
@@ -20,7 +24,7 @@ function About () {
                 <div className="mt-6 grid grid-cols-4 gap-2">
                     <AboutChevronDetails title="Nom" content="Yoan NOUGUÉ-RUIZ" />
                     <AboutChevronDetails title="Adresse mail" content={<a className={"text-blue-500"} href="mailto:yoan.nougueruiz@gmail.com">yoan.nougueruiz@gmail.com</a>} />
-                    <AboutChevronDetails title="Âge" content="22 ans" />
+                    <AboutChevronDetails title="Âge" content={age + " ans"} />
                     <AboutChevronDetails title="N° de téléphone" content="06 86 52 57 31" />
                     <AboutChevronDetails title="Ville" content="Nancy (54), Neufchâteau (88)" />
                     <AboutChevronDetails title="Diplôme" content="Licence Informatique" />
